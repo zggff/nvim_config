ffa26184cdaa99cab5b4d8a033dfcf6a45f0a348
Max Giga <gigamaximwachau@gmail.com>
2022-12-24 20:38:56 +0300

    fix issue with colorscheme in neovide

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -5,7 +5,7 @@ require("onedarkpro").setup({
         underline = true, -- Use the themes opinionated underline styles?
         undercurl = true, -- Use the themes opinionated undercurl styles?
         cursorline = true, -- Use cursorline highlighting?
-        transparency = true, -- Use a transparent background?
+        transparency = false, -- Use a transparent background?
         terminal_colors = true, -- Use the theme's colors for Neovim's :terminal?
         highlight_inactive_windows = true, -- When the window is out of focus, change the normal background?
     },
