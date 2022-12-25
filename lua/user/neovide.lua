722ff055afe493792b1ce9a73ebe78e3d3df0c4d
Max Giga <gigamaximwachau@gmail.com>
2022-12-26 00:06:49 +0300

    fix font size in neovide

--- lua/user/neovide.lua
+++ lua/user/neovide.lua
@@ -1,15 +1,42 @@
-if vim.g.neovide then
-    local keymapOpts = {
-        silent = true,
-        noremap = true,
-    }
-    vim.keymap.set({ "n", "v" }, "<D-v>", "\"*p", keymapOpts);
-    vim.keymap.set({ "n", "v" }, "<D-c>", "\"*y", keymapOpts);
-    vim.keymap.set({ "n", "v" }, "<D-x>", "\"*x", keymapOpts);
-    vim.keymap.set({ "i" }, "<D-v>", "<Esc>pa", keymapOpts);
-
-
-    vim.cmd [[
-        let g:neovide_hide_mouse_when_typing = v:true
-    ]]
+if not vim.g.neovide then
+    return
 end
+
+vim.g.neovide_hide_mouse_when_typing = true
+vim.g.gui_font_default_size = 20
+vim.g.fonts = {
+    "Cascadia Code",
+    "CaskaydiaCove Nerd Font"
+}
+vim.g.gui_font_size = vim.g.gui_font_default_size
+
+
+local opts = { noremap = true, silent = true }
+vim.keymap.set({ 'n', 'i' }, "<D-=>", function() ResizeGuiFont(1) end, opts)
+vim.keymap.set({ 'n', 'i' }, "<D-->", function() ResizeGuiFont(-1) end, opts)
+vim.keymap.set({ 'n', 'i' }, "<D-Bslash>", function() ResetGuiFont() end, opts)
+vim.keymap.set({ "n", "v" }, "<D-v>", "\"*p", opts);
+vim.keymap.set({ "n", "v" }, "<D-c>", "\"*y", opts);
+vim.keymap.set({ "n", "v" }, "<D-x>", "\"*x", opts);
+vim.keymap.set({ "i" }, "<D-v>", "<Esc>pa", opts);
+vim.keymap.set({ "t" }, "<D-v>", [[<C-\>p<C-n>]], opts);
+
+RefreshGuiFont = function()
+    local font_string = ""
+    for _, font in ipairs(vim.g.fonts) do
+        font_string = font_string .. string.format("%s:h%s,", font, vim.g.gui_font_size)
+    end
+    vim.opt.guifont = font_string:sub(1, -2)
+end
+
+ResizeGuiFont = function(delta)
+    vim.g.gui_font_size = vim.g.gui_font_size + delta
+    RefreshGuiFont()
+end
+
+ResetGuiFont = function()
+    vim.g.gui_font_size = vim.g.gui_font_default_size
+    RefreshGuiFont()
+end
+
+ResetGuiFont()
