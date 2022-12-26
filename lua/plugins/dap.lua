0a2f100e27330cc50bc357fac0de24158b36b8d7
Max Giga <gigamaximwachau@gmail.com>
2022-12-26 22:25:30 +0300

    debugging improvement: python support + auto detect cargo

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,6 +1,14 @@
-local dap_status_ok, dap = pcall(require, "dap")
-if not dap_status_ok then
-    return
+local dap = require("dap")
+
+function MySplit(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t = {}
+    for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+        table.insert(t, str)
+    end
+    return t
 end
 
 dap.adapters.lldb = {
@@ -15,6 +23,66 @@ dap.configurations.cpp = {
         type = 'lldb',
         request = 'launch',
         program = function()
+            local cwd = vim.fn.getcwd()
+            for _, val in ipairs(vim.fn.readdir(cwd)) do
+                if val == "a.out" then
+                    return cwd .. "/a.out"
+                end
+            end
+
+            return vim.fn.input('Path to executable: ', cwd .. '/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+        args = {},
+    },
+}
+dap.configurations.c = dap.configurations.cpp
+
+dap.configurations.rust = {
+    {
+        name = 'Launch',
+        type = 'lldb',
+        request = 'launch',
+        program = function()
+            local cwd = vim.fn.getcwd()
+            local buffer = vim.api.nvim_buf_get_name(0)
+            local buffer_split = MySplit(buffer, "/")
+            local filename = buffer_split[#buffer_split]:sub(1, -4)
+
+            local has_cargo = false;
+            local has_buffer = false;
+            for _, val in ipairs(vim.fn.readdir(cwd)) do
+                if val == "Cargo.toml" then
+                    has_cargo = true
+                elseif val == filename then
+                    has_buffer = true
+                end
+            end
+            local is_cargo_src = vim.fn.count(buffer, cwd .. "/src/") ~= 0
+            local is_cargo_bin = vim.fn.count(buffer, cwd .. "/src/bin/") ~= 0
+            if is_cargo_src ~= 0 and has_cargo then
+                local split = MySplit(cwd, "/")
+                local dirname = split[#split]
+                local target_directory = cwd .. "/target/debug/"
+                local file_exists = false
+                local target_file = dirname
+                if is_cargo_bin then
+                    target_file = filename
+                end
+                for _, val in ipairs(vim.fn.readdir(target_directory)) do
+                    if val == target_file then
+                        file_exists = true
+                        break
+                    end
+                end
+                if file_exists then
+                    return cwd .. "/target/debug/" .. target_file
+                end
+            end
+            if has_buffer then
+                return cwd .. "/" .. filename
+            end
             return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
         end,
         cwd = '${workspaceFolder}',
@@ -23,16 +91,9 @@ dap.configurations.cpp = {
     },
 }
 
--- If you want to use this for Rust and C, add something like this:
-
-dap.configurations.c = dap.configurations.cpp
-dap.configurations.rust = dap.configurations.cpp
-
-
 dap.adapters.haskell = {
     type = "executable",
     command = "haskell-debug-adapter",
-    -- args = { "--hackage-version=0.0.33.0" },
 }
 
 dap.configurations.haskell = {
@@ -58,8 +119,38 @@ dap.configurations.haskell = {
     },
 }
 
+dap.adapters.python = {
+    type = "executable",
+    command = "/opt/homebrew/bin/python3.11",
+    args = { '-m', 'debugpy.adapter' };
+}
+
+dap.configurations.python = {
+    {
+        -- The first three options are required by nvim-dap
+        type = 'python'; -- the type here established the link to the adapter definition: `dap.adapters.python`
+        request = 'launch';
+        name = "Launch file";
+        program = "${file}"; -- This configuration will launch the current file if used.
+        pythonPath = function()
+            -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+            -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+            -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+            local cwd = vim.fn.getcwd()
+            if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+                return cwd .. '/venv/bin/python'
+            elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+                return cwd .. '/.venv/bin/python'
+            else
+                return '/opt/homebrew/bin/python3.11'
+            end
+        end;
+    },
+}
+
 vim.cmd([[
-    nnoremap <silent> <F5> :lua require'dap'.continue()<CR>
+    nnoremap <silent> <F5>  :lua require'dap'.continue()<CR>
+    nnoremap <silent> <F8>  :lua require'dap'.toggle_breakpoint()<CR>>
     nnoremap <silent> <F10> :lua require'dap'.step_over()<CR>
     nnoremap <silent> <F11> :lua require'dap'.step_into()<CR>
     nnoremap <silent> <F12> :lua require'dap'.step_out()<CR>
