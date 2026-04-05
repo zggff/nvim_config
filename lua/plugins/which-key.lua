72a2f2251c78d5a53449de9d8e181ef410da73aa
zggff <gigamaximwachau@gmail.com>
2026-04-05 11:41:10 +0300

    remove all traces of lspsaga

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -8,16 +8,14 @@ return {
         { "<leader>f",  "<cmd>Telescope find_files<cr>",            desc = "Find files" },
         { "<leader>F",  "<cmd>Telescope live_grep<cr>",             desc = "Find Text" },
         { "<leader>b",  "<cmd>Telescope buffers<cr>",               desc = "Buffers" },
-        { "<leader>c",  "<cmd>bdelete!<CR>",                        desc = "Close Buffer" },
 
         { "<leader>l",  group = "LSP" },
-        { "<leader>ll", "<cmd>Lspsaga code_action<CR>",             desc = "Code actions" },
+        { "<leader>ll", vim.lsp.buf.code_action,                    desc = "Code actions" },
         { "<leader>lr", vim.lsp.buf.rename,                         desc = "rename" },
-        { "<leader>lo", "<cmd>LSoutlineToggle<CR>",                 desc = "outline" },
         { "<leader>ld", "<cmd>Telescope diagnostics<CR>",           desc = "diagnostics" },
         { "<leader>ls", "<cmd>Telescope lsp_document_symbols<CR>",  desc = "document symbols" },
         { "<leader>lS", "<cmd>Telescope lsp_workspace_symbols<CR>", desc = "workspace symbols" },
         { "<leader>lf", "<cmd>Format<CR>",                          desc = "format" },
-        { "<leader>d",  "<cmd>Lspsaga show_line_diagnostics<cr>",   desc = "Show diagnostics" },
+        { "<leader>d",  vim.diagnostic.open_float,                  desc = "Show diagnostics" },
     },
 }
